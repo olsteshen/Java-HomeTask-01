2823758fb6eaa546fe32287c158d0016f4a392d1
stesho01 <Listo4ek>
2021-12-03 19:42:42 +0200

    add task 01

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 74;
+        int b = 36;
+        int sum = a+b;
+        System.out.println("The sum of 74 and 36 is " + sum);
+    }
+}
